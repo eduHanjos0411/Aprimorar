7ad37c9d2c61e6e74570d05762fce9b87c880a40
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-12-09 10:30:39 -0300

    feat: initial commit; creating style varables and adding sections

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { Header } from './components/Header'
+import { Hero } from './components/Hero'
+import { About } from './components/QuemSomos'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    <About/>
+    </>
+  )
+}
+
+export default App
