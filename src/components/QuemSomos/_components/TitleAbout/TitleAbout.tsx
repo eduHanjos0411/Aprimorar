1f12286a09ac208d94980bef4592e8d02a12c694
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-12-09 10:33:03 -0300

    feat: creating about section and its skeleton

---
+++ src/components/QuemSomos/_components/TitleAbout/TitleAbout.tsx
@@ -0,0 +1,9 @@
+import './titleAbout.css'
+
+export function TitleAbout() {
+  return (
+    <h2 className="title-about">
+      O que é o Aprimorar?
+    </h2>
+  )
+}
---
+++ src/components/QuemSomos/index.tsx
@@ -0,0 +1,18 @@
+import { TitleAbout } from "./_components/TitleAbout/TitleAbout";
+
+export function About() {
+  return (
+    <section id="about">
+      <header>
+        <TitleAbout/>
+      </header>
+      <article>
+        
+        <p>
+
+        </p>
+        <img/>
+      </article>
+    </section>
+  )
+}
