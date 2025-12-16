fc608419d280ee1c3ad8c2a54e7f9533fc641e2f
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-12-16 12:50:54 -0300

    fix: alterando formato de componente para melhor legibilidade e SEO

---
+++ src/components/About/_components/TitleAbout/TitleAbout.tsx
@@ -0,0 +1,9 @@
+import './titleAbout.css'
+
+export function TitleAbout() {
+  return (
+    <h2 id="about-title" className="title-about">
+      Aprimorar: Espaço Multidisciplinar de Aprendizagem
+    </h2>
+  );
+}
