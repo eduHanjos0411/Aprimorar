6bd6f8473b6a765dbf8f1845ad617c94ca4e6116
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-12-16 17:04:50 -0300

    feat: adicionando novas sessões ao app

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { About } from './components/About'
 import { useReveal } from './hooks/useReveal'
 import { Servicos } from './components/Servicos/Index'
 import { Depoimentos } from './components/Depoimentos/Depoimentos'
+import { Contato } from './components/Contatos/_components/Contato'
+import { Footer } from './components/Footer/Footer'
 
 function App() {
   useReveal()
@@ -19,6 +21,8 @@ function App() {
         <About />
         <Servicos/>
         <Depoimentos/>
+        <Contato/>
+        <Footer/>
       </main>
     </>
   );
