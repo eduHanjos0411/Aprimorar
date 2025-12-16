67c22f0651c954df8ed2d221e0708d58b8f89fe8
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-12-16 12:51:51 -0300

    fix: alterando componente para melhor legibilidade e SEO

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,10 +1,33 @@
-import { LogoHero } from './_components/LogoHero'
-import './hero.css'
+import { useReveal } from "../../hooks/useReveal";
+import { CtaButton } from "../CtaButton/CtaButton"
+import { LogoHero } from "./_components/LogoHero"
+import "./heroStyle.css"
 
 export function Hero() {
+    useReveal()
+
   return (
-    <section id="inicio" className="hero-section">
-      <LogoHero/>
+    <section
+      id="inicio"
+      className="hero-section"
+      aria-labelledby="hero-title"
+    >
+      <article className="texto-hero reveal">
+        <LogoHero />
+
+        <h1 id="hero-title">
+          Eleve seus estudos com acompanhamento educacional individualizado
+        </h1>
+
+        <p className="hero-subtitle">
+          No Aprimorar, desenvolvemos métodos de estudo eficientes para
+          potencializar o aprendizado e os resultados acadêmicos.
+        </p>
+
+        <span>
+          <CtaButton />
+        </span>
+      </article>
     </section>
-  )
+  );
 }
